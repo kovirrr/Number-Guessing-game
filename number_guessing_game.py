5037cdab297b92daa3b4b8ce088d0b4095185426
Kovi Ressler <koviressler@gmail.com>
2024-07-23 11:44:17 -0400

    kovi is aweesome

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -58,7 +58,7 @@ for i in range(remaining_attempts):
         remaining_attempts = remaining_attempts + 1
 
 # Display game over message
-print("\nGame over")
+print("\nKovi is super cool")
 
 # Check if the player lost the game
 if remaining_attempts == 0:
